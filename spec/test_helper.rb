492caebba96e5395a1226501e9d7434782e711bb
dominik <dominik.alberski@gmail.com>
2023-10-04 16:21:34 +0200

    test: configure test

---
+++ spec/test_helper.rb
@@ -0,0 +1,21 @@
+require_relative 'spec_helper'
+require_relative '../app'
+# Exit if the environment is not set.
+exit unless ENV['RACK_ENV'] == 'test'
+
+require 'database_cleaner/active_record'
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
