294a16c3996457b58fcf79b906da4e8d4162d29a
dominik <dominik.alberski@gmail.com>
2023-10-04 16:20:06 +0200

    build: reconfigure application

--- Rakefile
+++ Rakefile
@@ -1,2 +1,11 @@
 require 'sinatra/activerecord/rake'
+require 'rspec/core/rake_task'
 require './app'
+ENV['RACK_ENV'] = 'development'
+
+# RSpec Rake task for running tests
+RSpec::Core::RakeTask.new(:spec => "db:test:prepare") do
+  ENV['RACK_ENV'] = 'test'
+end
+
+task default: :spec
--- app.rb
+++ app.rb
@@ -1,16 +1,24 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/json'
+require 'yaml'
 require "zeitwerk"
 
 loader = Zeitwerk::Loader.new
-loader.log!
 loader.push_dir(File.join(__dir__, 'models'))
 loader.push_dir(File.join(__dir__, 'controllers'))
 loader.push_dir(File.join(__dir__, 'config'))
 loader.setup
 loader.eager_load
 
-set :database, {adapter: "sqlite3", database: "db/development.sqlite3"}
+# Load database configurations based on the environment.
+# By default, Sinatra's environment is set to development.
+# change `ENV['RACK_ENV']` to different ones when needed.
+db_config = YAML.load_file('config/database.yml')[Sinatra::Base.environment.to_s]
 
-Router.run!
+set :database, db_config
+
+# Start the application if this file is executed directly.
+if __FILE__ == $0
+  Router.run!
+end
