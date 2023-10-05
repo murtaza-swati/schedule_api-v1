a7cc107e00c2e914c9ee6223f85598e7beebeabd
dominik <dominik.alberski@gmail.com>
2023-10-05 17:05:00 +0200

    feat: improve doctor model validations

--- models/doctor.rb
+++ models/doctor.rb
@@ -15,9 +15,20 @@
 class Doctor < ActiveRecord::Base
   has_many :appointments
 
+  # Validates the presence of name, work_start_time, work_end_time, slot_duration, and break_duration.
   validates :name, :work_start_time, :work_end_time, :slot_duration,
     :break_duration, presence: true
 
+  # Validates that work_start_time is before work_end_time.
+  validate :work_start_time_before_work_end_time
+
+  # Validates that slot_duration is shorter than the doctor's working hours.
+  validate :slot_duration_is_shorter_than_working_hours
+
+  # Checks if the appointment time is within the doctor's working hours.
+  #
+  # @param appointment_time [Time] the time of the appointment
+  # @return [Boolean] true if the appointment is within working hours, false otherwise
   def appointment_within_working_hours?(appointment_time)
     appt_time_as_time = Time.new(
       work_start_time.year,
@@ -34,5 +45,27 @@ class Doctor < ActiveRecord::Base
     # Check if both start and end times are within doctor's working hours.
     appt_time_as_time.between?(work_start_time, work_end_time) &&
       appointment_end_time.between?(work_start_time, work_end_time)
+  end
+
+  private
+
+  # Validates that work_start_time is before work_end_time.
+  def work_start_time_before_work_end_time
+    return unless work_start_time && work_end_time
+
+    if work_start_time >= work_end_time
+      errors.add(:work_start_time, "must be before work end time")
+    end
+  end
+
+  # Validates that slot_duration is shorter than the doctor's working hours.
+  def slot_duration_is_shorter_than_working_hours
+    return unless work_start_time && work_end_time && slot_duration
+
+    work_duration = (work_end_time - work_start_time) / 60
+
+    if slot_duration >= work_duration
+      errors.add(:slot_duration, "must be shorter than working hours")
+    end
   end
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -15,20 +15,85 @@
 require "test_helper"
 
 RSpec.describe Doctor do
+  let(:start_time) { Time.parse("9:00 AM UTC") }
+  let(:end_time) { Time.parse("5:00 PM UTC") }
+  let(:slot_duration) { 55 }
+  let(:break_duration) { 5 }
+  let(:name) { "Dr. John Doe" }
+
   let(:doctor) do
     Doctor.new(
-      name: "Dr. John Doe",
+      name: name,
       work_start_time: start_time,
       work_end_time: end_time,
-      slot_duration: 55,
-      break_duration: 5
+      slot_duration: slot_duration,
+      break_duration: break_duration
     )
   end
 
-  describe "#appointment_within_working_hours?" do
-    let(:start_time) { Time.parse("9:00 AM UTC") }
-    let(:end_time) { Time.parse("5:00 PM UTC") }
+  describe "validations" do
+    subject { doctor }
 
+    it { is_expected.to be_valid }
+
+    context "when name is not present" do
+      let(:name) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when work_start_time is missing" do
+      let(:start_time) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when work_end_time is missing" do
+      let(:end_time) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when slot_duration is missing" do
+      let(:slot_duration) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when break_duration is missing" do
+      let(:break_duration) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when work_start_time is after work_end_time" do
+      let(:start_time) { Time.parse("5:00 PM UTC") }
+      let(:end_time) { Time.parse("9:00 AM UTC") }
+
+      it "is invalid" do
+        expect(subject).to be_invalid
+      end
+    end
+
+    context "when work_start_time is equal to work_end_time" do
+      let(:start_time) { Time.parse("5:00 PM UTC") }
+      let(:end_time) { Time.parse("5:00 PM UTC") }
+
+      it "is invalid" do
+        expect(subject).to be_invalid
+      end
+    end
+
+    context "when slot_duration is greater than work duration" do
+      let(:slot_duration) { 9 * 60 }
+
+      it "is invalid" do
+        expect(subject).to be_invalid
+      end
+    end
+  end
+
+  describe "#appointment_within_working_hours?" do
     context "within working hours" do
       let(:appointment_time) { Time.parse("11:00 AM UTC") }
       it "returns true" do
