c6aa385c87dc5b9dab35986e4b9a79d957e4d29c
dominik <dominik.alberski@gmail.com>
2023-10-11 17:10:38 +0200

    refactor: use schema for coertion

--- controllers/appointments_controller.rb
+++ controllers/appointments_controller.rb
@@ -1,6 +1,6 @@
 class AppointmentsController
   def self.call(action, params)
-    params = AppointmentParams.call(params)
+    params = ParamsSchema.call(params)
     if params.errors.any?
       {error: params.errors.to_h}
     else
@@ -18,22 +18,16 @@ class AppointmentsController
   def index
     AvailableSlotService.call(
       doctor,
-      start_date: parse_date(params[:start_date]),
-      end_date: parse_date(params[:end_date])
+      start_date: params[:start_date],
+      end_date: params[:end_date]
     )
   end
 
   def create
-    CreateAppointmentService.call(doctor, params)
+    CreateAppointmentsService.call(doctor, params)
   end
 
   private
 
   attr_accessor :doctor, :params
-
-  def parse_date(date)
-    if date.is_a?(String)
-      Date.parse(date)
-    end
-  end
 end
