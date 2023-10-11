02571d8e6bad0d9162874599d13ba66d18f6dabe
dominik <dominik.alberski@gmail.com>
2023-10-11 17:11:00 +0200

    fix: add missing values to schema

--- controllers/appointments_controller/params_schema.rb
+++ controllers/appointments_controller/params_schema.rb
@@ -3,6 +3,8 @@ require "dry-schema"
 class AppointmentsController
   ParamsSchema = Dry::Schema.Params do
     required(:doctor_id).filled(:integer)
+    optional(:start_date).filled(:date)
+    optional(:end_date).filled(:date)
     optional(:appointment_id).filled(:integer)
     optional(:appointments).value(:array).each do
       schema do
