aa72906d89d752ff401912a39c963705a99daa20
dominik <dominik.alberski@gmail.com>
2023-10-12 11:08:13 +0200

    style: fix standard offences

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AppointmentsController do
   describe ".call" do
     context "when doctor is not found" do
       it "returns an error message" do
-        expect(AppointmentsController.call(:action, {})).to eq({error: {:doctor_id=>["is missing"]}})
+        expect(AppointmentsController.call(:action, {})).to eq({error: {doctor_id: ["is missing"]}})
       end
     end
 
